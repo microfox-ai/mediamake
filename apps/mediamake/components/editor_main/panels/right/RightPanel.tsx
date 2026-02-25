3f5c339d925a880b673f7733907ca009acab8541
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-02-26 02:47:42 +0530

    feat: enhance RightPanel layout by adding flexible container for EditorProps to improve rendering and overflow handling

--- apps/mediamake/components/editor_main/panels/right/RightPanel.tsx
+++ apps/mediamake/components/editor_main/panels/right/RightPanel.tsx
@@ -36,7 +36,13 @@ export function RightPanel() {
   const renderPatternContent = () => {
     switch (currentPattern) {
       case 'edit':
-        return <EditorProps />;
+        return (
+          <div className="flex h-full flex-col min-h-0">
+            <div className="flex-1 min-h-0 overflow-auto">
+              <EditorProps />
+            </div>
+          </div>
+        );
       case 'make':
       case 'media':
       case 'render':
