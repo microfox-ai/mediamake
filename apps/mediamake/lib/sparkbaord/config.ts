f319d7f0acefc8bf7728565b1932b0c73b315429
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-03-05 14:47:07 +0530

    chore: remove Sparkboard configuration and type definitions to streamline codebase and improve maintainability

--- apps/mediamake/lib/sparkbaord/config.ts
+++
@@ -1,8 +0,0 @@
-const AI_ANALYSIS_BASE_URL =
-  'https://506abypwqe.execute-api.us-east-1.amazonaws.com/prod';
-// 'http://localhost:4000/dev';
-
-export const AI_ANALYSIS_CONFIG = {
-  baseUrl: AI_ANALYSIS_BASE_URL,
-  apiKey: process.env.SPARKBOARD_API_KEY,
-};
