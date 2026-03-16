c6899a7e3e4462f4978c2fbcb90c7a5a2c1432fd
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-03-16 22:54:00 +0530

    refactor: remove unused AWS regions from configuration

--- apps/mediamake/config.mjs
+++ apps/mediamake/config.mjs
@@ -16,19 +16,9 @@ export const DEFAULT_REGION = REGION;
 export const AWS_REGIONS = [
   'us-east-1', // US East (N. Virginia)
   'us-east-2', // US East (Ohio)
-  'us-west-1', // US West (N. California)
-  'us-west-2', // US West (Oregon)
-  'eu-central-1', // Europe (Frankfurt)
   'eu-west-1', // Europe (Ireland)
-  'eu-west-2', // Europe (London)
-  'eu-west-3', // Europe (Paris)
-  'eu-north-1', // Europe (Stockholm)
   'ap-south-1', // Asia Pacific (Mumbai)
   'ap-southeast-1', // Asia Pacific (Singapore)
-  'ap-southeast-2', // Asia Pacific (Sydney)
-  'ap-northeast-1', // Asia Pacific (Tokyo)
-  'ap-northeast-2', // Asia Pacific (Seoul)
-  'ap-northeast-3', // Asia Pacific (Osaka)
 ];
 
 /**
@@ -38,19 +28,9 @@ export const AWS_REGION_OPTIONS = AWS_REGIONS.map((value) => {
   const labelMap = {
     'us-east-1': 'US East (N. Virginia)',
     'us-east-2': 'US East (Ohio)',
-    'us-west-1': 'US West (N. California)',
-    'us-west-2': 'US West (Oregon)',
-    'eu-central-1': 'Europe (Frankfurt)',
     'eu-west-1': 'Europe (Ireland)',
-    'eu-west-2': 'Europe (London)',
-    'eu-west-3': 'Europe (Paris)',
-    'eu-north-1': 'Europe (Stockholm)',
     'ap-south-1': 'Asia Pacific (Mumbai)',
     'ap-southeast-1': 'Asia Pacific (Singapore)',
-    'ap-southeast-2': 'Asia Pacific (Sydney)',
-    'ap-northeast-1': 'Asia Pacific (Tokyo)',
-    'ap-northeast-2': 'Asia Pacific (Seoul)',
-    'ap-northeast-3': 'Asia Pacific (Osaka)',
   };
 
   return {
